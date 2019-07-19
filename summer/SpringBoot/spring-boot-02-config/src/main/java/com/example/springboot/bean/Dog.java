3b091adce22985fab42da78c10b64fd3dd6606ab
lala <2665687186@qq.com>
2019-07-19 21:44:52 +0800

    第二个程序

---
+++ summer/SpringBoot/spring-boot-02-config/src/main/java/com/example/springboot/bean/Dog.java
@@ -0,0 +1,38 @@
+package com.example.springboot.bean;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+public class Dog {
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private Integer age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+
+}
