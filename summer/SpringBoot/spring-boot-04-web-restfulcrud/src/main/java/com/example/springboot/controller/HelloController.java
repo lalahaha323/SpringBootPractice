a9775a98436dbeb7c6879a18f84120e9ce1c363c
lala <2665687186@qq.com>
2019-07-23 21:31:43 +0800

    使用Thymeleaf

--- summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/controller/HelloController.java
+++ summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/controller/HelloController.java
@@ -3,13 +3,20 @@ package com.example.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success() {
+        //classpath:/templates/success.html
+        return "success";
     }
 }
