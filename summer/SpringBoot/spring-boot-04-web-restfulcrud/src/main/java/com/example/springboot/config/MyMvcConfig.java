ef03b8f56448bb8f1a90b35b3a79caba19584838
lala <2665687186@qq.com>
2019-07-24 11:08:29 +0800

    出来主页面

--- summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/config/MyMvcConfig.java
+++ summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/config/MyMvcConfig.java
@@ -1,17 +1,15 @@
 package com.example.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
+
 
 
 //使用WebMvcConfigurer扩展SpringMvc使用的功能
 @Configuration
-public class MyMvcConfig extends WebMvcConfigurationSupport {
+public class MyMvcConfig implements WebMvcConfigurer {
     @Override
-    protected void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry) {
 //        super.addViewControllers(registry);
         //浏览器发送 /lala 请求来到success页面
         registry.addViewController("/lala").setViewName("success");
