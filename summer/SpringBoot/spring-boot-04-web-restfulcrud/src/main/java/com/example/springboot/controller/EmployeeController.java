73845d2b362545c184935d0e90f5ca6e6ecb24f4
lala <2665687186@qq.com>
2019-07-25 10:57:41 +0800

    员工添加

--- summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/controller/EmployeeController.java
+++ summer/SpringBoot/spring-boot-04-web-restfulcrud/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.example.springboot.controller;
 
 
+import com.example.springboot.dao.DepartmentDao;
 import com.example.springboot.dao.EmployeeDao;
+import com.example.springboot.entities.Department;
 import com.example.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -15,6 +18,9 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
     //查询所有员工返回列表页面
     @GetMapping("/emps")
@@ -28,5 +34,28 @@ public class EmployeeController {
         // thymeleaf默认会拼串
         // classpath：/templates/emp/list.html
         return "emp/list";
+    }
+
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        //来到添加页面,查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "/emp/add";
+    }
+
+    //员工添加
+    //SpringMvc自动将请求参数和入参对象的属性进行一一绑定：要求请求参数的名字和JavaBean入参的对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        //来到员工列表页面
+
+        System.out.println("保存的员工信息: " + employee);
+        //保存员工
+        employeeDao.save(employee);
+        //rediect：表示重定向到一个地址  /代表当前项目路径
+        //forward:表示转发到一个地址
+        return "redirect:/emps";
     }
 }
