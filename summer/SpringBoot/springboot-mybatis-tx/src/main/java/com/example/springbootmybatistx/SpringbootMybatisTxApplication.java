50e3a7fb14ae9eae205d14b56ee4a69537af26cc
lala <2665687186@qq.com>
2019-08-02 01:48:29 +0800

    编程式事务管理

--- summer/SpringBoot/springboot-mybatis-tx/src/main/java/com/example/springbootmybatistx/SpringbootMybatisTxApplication.java
+++ summer/SpringBoot/springboot-mybatis-tx/src/main/java/com/example/springbootmybatistx/SpringbootMybatisTxApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.springbootmybatistx.dao")
 public class SpringbootMybatisTxApplication {
 
     public static void main(String[] args) {
