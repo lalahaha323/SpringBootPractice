fddc4f0f31b3a4fc49fe3776cc3a3a923c2a0f6b
lala <2665687186@qq.com>
2019-08-02 09:42:44 +0800

    基于AspectJ的声明式事务管理

--- summer/SpringBoot/springboot-mybatis-tx/src/main/java/com/example/springbootmybatistx/service/OrdersService.java
+++ summer/SpringBoot/springboot-mybatis-tx/src/main/java/com/example/springbootmybatistx/service/OrdersService.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
-@Service
+
 public class OrdersService {
 
     private OrdersDao ordersDao;
@@ -17,29 +17,9 @@ public class OrdersService {
         this.ordersDao = ordersDao;
     }
 
-    private TransactionTemplate transactionTemplate;
-
-    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-        this.transactionTemplate = transactionTemplate;
-    }
-
     public void accountMoney() {
-        transactionTemplate.execute(new TransactionCallback<Object>() {
-
-            @Override
-            public Object doInTransaction(TransactionStatus status) {
-                Object result = null;
-                try {
-                    ordersDao.addMoney();
-                    int i = 10 / 0;
-                    ordersDao.reduceMoney();
-                } catch (Exception e) {
-                    status.setRollbackOnly();
-                    result = false;
-                    System.out.println("Transfer Error!");
-                }
-                return result;
-            }
-        });
+        ordersDao.addMoney();
+        //int i = 10 / 0;
+        ordersDao.reduceMoney();
     }
 }
