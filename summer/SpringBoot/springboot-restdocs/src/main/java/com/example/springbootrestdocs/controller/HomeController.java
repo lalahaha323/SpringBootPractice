36f1ad517129ca0f936b2ec315d88815e3fe5a01
lala <2665687186@qq.com>
2019-08-08 11:53:36 +0800

    springboot整合restdoc

---
+++ summer/SpringBoot/springboot-restdocs/src/main/java/com/example/springbootrestdocs/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springbootrestdocs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author lala
+ */
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello World");
+    }
+}
