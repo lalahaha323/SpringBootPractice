e582623fbf7e8f5e0ac8b579af9a7db1fa01489a
lala <2665687186@qq.com>
2019-08-09 16:58:31 +0800

    授权登录

---
+++ summer/SpringBoot/community/src/main/java/com/example/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author lala
+ */
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
