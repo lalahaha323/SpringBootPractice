6bbe2e7d395b08f4b0a57c3237fee9c64a764369
lala <2665687186@qq.com>
2019-08-10 14:43:43 +0800

    授权登录

--- summer/SpringBoot/community/src/main/java/com/example/community/controller/AuthorizeController.java
+++ summer/SpringBoot/community/src/main/java/com/example/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.community.dto.AccessTokenDTO;
 import com.example.community.dto.GithubUser;
 import com.example.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,15 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
