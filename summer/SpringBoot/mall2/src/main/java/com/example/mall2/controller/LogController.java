64ce2a029c80e3a41255ec493c7e7bc94aa8fb85
lala <2665687186@qq.com>
2019-09-26 23:08:59 +0800

    修改md5加密的bug

--- summer/SpringBoot/mall2/src/main/java/com/example/mall2/controller/LogController.java
+++ summer/SpringBoot/mall2/src/main/java/com/example/mall2/controller/LogController.java
@@ -11,6 +11,7 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
